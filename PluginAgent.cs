74cc02ea978c001022bb5231763d4f2c25df927f
Mark Studer <mark@beyondcomputing.org>
2015-11-08 00:17:13 -0700

    Initial Working version.  Testing out different WMI options for pulling VM data.

--- PluginAgent.cs
+++ PluginAgent.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Reflection;
 using NewRelic.Platform.Sdk;
+using NewRelic.Platform.Sdk.Utils;
+using System.Management;
 
 namespace Org.BeyondComputing.NewRelic.HyperV
 {
@@ -9,6 +11,9 @@ namespace Org.BeyondComputing.NewRelic.HyperV
     {
         // Name of Agent
         private string name;
+
+        // Provides logging for Plugin
+        private Logger log = Logger.GetLogger(typeof(PluginAgent).Name);
 
         /// <summary>
         /// Constructor for Agent Class
@@ -60,9 +65,119 @@ namespace Org.BeyondComputing.NewRelic.HyperV
         /// </summary>
         public override void PollCycle()
         {
-            Console.WriteLine("Poll Cycle not implemented");
+            try
+            {
+                log.Info($"Collecting information from server: {this.name}");
+                ManagementObjectCollection vmDetails = hyperv.GetVMDetails(this.name);
+
+                foreach (ManagementObject vm in vmDetails)
+                {
+                    // Exclude host from metrics - just want VM's
+                    if (vm["Description"].ToString() != "Microsoft Hosting Computer System")
+                    {
+                        // Report Metrics to New Relic
+                        ReportVMMetrics(vm);
+                        ReportVMUptime(vm);
+                        ReportReplicationHealth(vm);
+                        ReportVMHealth(vm);
+                    }
+                }
+            }
+            catch
+            {
+                log.Error("Unable to fetch information from the Hyper-v host '{0}'", this.name);
+            }
         }
 
         #endregion
+
+        private void ReportVMMetrics(ManagementObject vm)
+        {
+            // Name of VM
+            string name = vm["ElementName"].ToString();
+
+            // Create Settings
+            ManagementObject vmSettings = hyperv.GetVirtualSystemSettingData(this.name, vm["__PATH"].ToString());
+            ManagementObject vsms = hyperv.GetVSMS(this.name);
+
+            // Get VM Settings
+            ManagementBaseObject inParams = vsms.GetMethodParameters("GetSummaryInformation");
+            inParams["SettingData"] = new string[1] { vmSettings["__PATH"].ToString() };
+
+            // Request CPU Information
+            inParams["RequestedInformation"] = new int[4] { 4, 100, 101, 103};
+
+            ManagementBaseObject outParams = vsms.InvokeMethod("GetSummaryInformation", inParams, null);
+            ManagementBaseObject[] value = (ManagementBaseObject[])outParams.GetPropertyValue("SummaryInformation");
+
+            UInt16 state = (UInt16)value[0].GetPropertyValue("EnabledState");
+            ReportMetric($"vms/{name}/numberofprocessors", "procs", (UInt16)value[0].GetPropertyValue("NumberOfProcessors"));
+
+            if (state == 2)
+            {
+                ReportMetric($"vms/{name}/processorload", "percent", (UInt16)value[0].GetPropertyValue("ProcessorLoad"));
+                ReportMetric($"vms/{name}/memoryused", "mibibytes", (UInt64)value[0].GetPropertyValue("MemoryUsage"));
+            }
+
+            // Cleanup
+            inParams.Dispose();
+            outParams.Dispose();
+            vmSettings.Dispose();
+            vsms.Dispose();
+        }
+
+        private void ReportVMUptime(ManagementObject vm)
+        {
+            // Name of VM
+            string name = vm["ElementName"].ToString();
+
+            // Uptime of VM
+            UInt64 UpTimeDays = UInt64.Parse(vm["OnTimeInMilliseconds"].ToString()) / (1000 * 60 * 60 * 24);
+            UInt64 UpTimeHours = UInt64.Parse(vm["OnTimeInMilliseconds"].ToString()) / (1000 * 60 * 60);
+            UInt64 UpTimeMinutes = UInt64.Parse(vm["OnTimeInMilliseconds"].ToString()) / (1000 * 60);
+
+            ReportMetric($"vms/{name}/UpTimeInDays", "Days", UpTimeDays);
+            ReportMetric($"vms/{name}/UpTimeInHours", "Hours", UpTimeHours);
+            ReportMetric($"vms/{name}/UpTimeInMinutes", "Minutes", UpTimeMinutes);
+        }
+
+        private void ReportVMHealth(ManagementObject vm)
+        {
+            // Name of VM
+            string name = vm["ElementName"].ToString();
+
+            // Health of VM
+            hyperv.NewRelicMetric HealthMetric;
+            HealthMetric = hyperv.GetHealthState(vm);
+            ReportMetric($"vms/{name}/health", "errors", HealthMetric.Metric);
+        }
+
+        private void ReportReplicationHealth(ManagementObject vm)
+        {
+            // Name of VM
+            string name = vm["ElementName"].ToString();
+
+            // Get Replication Mode
+            hyperv.NewRelicMetric ReplicationMode;
+            ReplicationMode = hyperv.GetReplicationMode(vm);
+
+            // Get Replication Health Status
+            hyperv.NewRelicMetric ReplicationHealth;
+            ReplicationHealth = hyperv.GetReplicationHealth(vm);
+
+            if (ReplicationMode.Metric == 1)
+            {
+                // Primary Replication Node
+                log.Info($"Primary Node {name} - Health: {ReplicationHealth.Description}");
+                ReportMetric($"replication/primary/{name}/health", "errors", ReplicationHealth.Metric);
+            }
+            else if (ReplicationMode.Metric != 0)
+            {
+                // Secondary Replication Node
+                log.Info($"Secondary Node {name} - Health: {ReplicationHealth.Description}");
+                ReportMetric($"replication/secondary/{name}/health", "errors", ReplicationHealth.Metric);
+            }
+
+        }
     }
 }
---
+++ hyperv.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using NewRelic.Platform.Sdk;
+using NewRelic.Platform.Sdk.Utils;
+
+namespace Org.BeyondComputing.NewRelic.HyperV
+{
+    class hyperv
+    {
+        public struct NewRelicMetric
+        {
+            public string Description;
+            public float Metric;
+        }
+
+        public static ManagementObjectCollection GetVMDetails(string Server)
+        {
+            // Connect to the Hyper-V namespace
+            ManagementScope manScope = new ManagementScope($@"\\{Server}\root\virtualization\v2");
+
+            // Grab all VM info from msvm_ComputerSystem
+            ObjectQuery queryObj = new ObjectQuery("SELECT * FROM Msvm_ComputerSystem");
+            ManagementObjectSearcher vmSearcher = new ManagementObjectSearcher(manScope, queryObj);
+            ManagementObjectCollection vmCollection = vmSearcher.Get();
+
+            // Return Collection
+            return vmCollection;
+        }
+
+        public static ManagementObject GetVSMS(string Server)
+        {
+            ManagementScope scope = new ManagementScope($@"\\{Server}\root\virtualization\v2");
+            scope.Connect();
+
+            ManagementPath wmiPath = new ManagementPath("Msvm_VirtualSystemManagementService");
+            ManagementClass serviceClass = new ManagementClass(scope, wmiPath, null);
+            ManagementObjectCollection services = serviceClass.GetInstances();
+
+            ManagementObject serviceObject = null;
+
+            foreach (ManagementObject service in services)
+            {
+                serviceObject = service;
+            }
+            return serviceObject;
+        }
+
+        public static ManagementObject GetVirtualSystemSettingData(string Server, string VMpath)
+        {
+            // Connect to the Hyper-V namespace
+            ManagementScope manScope = new ManagementScope($@"\\{Server}\root\virtualization\v2");
+
+            // Grab settings for the specified VM
+            ObjectQuery queryObj = new ObjectQuery($"ASSOCIATORS OF {{{VMpath}}} WHERE resultClass = Msvm_VirtualSystemsettingData");
+            ManagementObjectSearcher vmSearcher = new ManagementObjectSearcher(manScope, queryObj);
+            ManagementObjectCollection vmCollection = vmSearcher.Get();
+            ManagementObject mo = vmCollection.OfType<ManagementObject>().FirstOrDefault();
+
+            return mo;
+        }
+
+        public static NewRelicMetric GetHealthState(ManagementObject VM)
+        {
+            NewRelicMetric Metric = new NewRelicMetric();
+            int HealthCode = Convert.ToInt32(VM["HealthState"]);
+
+            if(HealthCode == 5)
+            {
+                Metric.Metric = 0;
+                Metric.Description = "Healthy";
+            }
+            else if(HealthCode == 20)
+            {
+                Metric.Metric = 1;
+                Metric.Description = "Major Failure";
+            }
+            else
+            {
+                Metric.Metric = 2;
+                Metric.Description = "Critical Failure";
+            }
+
+            return Metric;
+        }
+
+        public static NewRelicMetric GetReplicationHealth(ManagementObject VM)
+        {
+            NewRelicMetric Metric = new NewRelicMetric();
+            int HealthCode = Convert.ToInt32(VM["ReplicationHealth"]);
+
+            if (HealthCode == 0)
+            {
+                Metric.Metric = 0;
+                Metric.Description = "Not applicable";
+            }
+            else if (HealthCode == 1)
+            {
+                Metric.Metric = 0;
+                Metric.Description = "Ok";
+            }
+            else if (HealthCode == 2)
+            {
+                Metric.Metric = 1;
+                Metric.Description = "Warning";
+            }
+            else
+            {
+                Metric.Metric = 2;
+                Metric.Description = "Critical";
+            }
+
+            return Metric;
+        }
+
+        public static NewRelicMetric GetReplicationMode(ManagementObject VM)
+        {
+            NewRelicMetric Metric = new NewRelicMetric();
+            int HealthCode = Convert.ToInt32(VM["ReplicationMode"]);
+
+            if (HealthCode == 0)
+            {
+                Metric.Metric = 0;
+                Metric.Description = "None";
+            }
+            else if (HealthCode == 1)
+            {
+                Metric.Metric = 1;
+                Metric.Description = "Primary";
+            }
+            else if (HealthCode == 2)
+            {
+                Metric.Metric = 2;
+                Metric.Description = "Recovery";
+            }
+            else if (HealthCode == 3)
+            {
+                Metric.Metric = 3;
+                Metric.Description = "Replica";
+            }
+            else if (HealthCode == 4)
+            {
+                Metric.Metric = 4;
+                Metric.Description = "Extended replica";
+            }
+
+            return Metric;
+        }
+
+    }
+}
